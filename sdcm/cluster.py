8e5a3193e04b10f606f256822188549faae24092
Amos Kong <amos@scylladb.com>
2019-10-28 22:44:34 +0800

    Revert "switch prepare of scylla monitor to python2 (#1431)"
    This reverts commit 5c00492bab5ff8aa61f698a5efaea7f078f2c719.

--- sdcm/cluster.py
+++ sdcm/cluster.py
@@ -3402,9 +3402,10 @@ class BaseMonitorSet(object):
             node.update_repo_cache()
             prereqs_script = dedent("""
                 yum install -y unzip wget
-                easy_install pip
-                pip install --upgrade pip
-                pip install pyyaml
+                yum install -y python36
+                yum install -y python36-pip
+                python3 -m pip install --upgrade pip
+                python3 -m pip install pyyaml
                 curl -fsSL get.docker.com -o get-docker.sh
                 sh get-docker.sh
                 systemctl start docker
